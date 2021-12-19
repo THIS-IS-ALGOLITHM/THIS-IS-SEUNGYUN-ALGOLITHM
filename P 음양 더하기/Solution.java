230390b80e1e271bace51c6ff592772c664c89d6
이승윤 <yyuunn17@naver.com>
2021-12-19 22:23:41 +0900

    2021-12-19

---
+++ P 음양 더하기/Solution.java
@@ -0,0 +1,24 @@
+package 음양더하기;
+
+public class Solution {
+    public static void main(String[] args) {
+        //test1
+        int[] absolutes1 = {4,7,12};
+        boolean[] signs1 = {true,false,true};
+        System.out.println(solution(absolutes1, signs1));
+
+        //test2
+        int[] absolutes2 = {1,2,3};
+        boolean[] signs2 = {false,false,true};
+        System.out.println(solution(absolutes2, signs2))    ;
+    }
+
+    public static int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i = 0; i < absolutes.length; i++) {
+            if (signs[i]) answer += absolutes[i];
+            else answer -= absolutes[i];
+        }
+        return answer;
+    }
+}
