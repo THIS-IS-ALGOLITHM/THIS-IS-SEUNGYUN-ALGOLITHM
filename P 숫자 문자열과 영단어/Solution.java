eddf65c2cc9d7057eb006f6ec1c088f4f6067dd0
이승윤 <yyuunn17@naver.com>
2021-12-19 21:07:20 +0900

    -

--- P 숫자 문자열과 영단어/Solution.java
+++ P 숫자 문자열과 영단어/Solution.java
@@ -11,17 +11,11 @@ public class Solution {
     }
 
     public static int solution(String s) {
-        s = s.replaceAll("zero", "0");
-        s = s.replaceAll("one","1");
-        s = s.replaceAll("two","2");
-        s = s.replaceAll("three","3");
-        s = s.replaceAll("four","4");
-        s = s.replaceAll("five","5");
-        s = s.replaceAll("six","6");
-        s = s.replaceAll("seven","7");
-        s = s.replaceAll("eight","8");
-        s = s.replaceAll("nine","9");
+        String[] number = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
+        for(int i = 0; i < 10; i++) {
+            s = s.replaceAll(number[i], String.valueOf(i));
+        }
 
         return Integer.parseInt(s);
     }
